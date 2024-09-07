f5355fba19707b03c151363f00df7be25fc47766
VibhinnS <vibhinnsinghal25@gmail.com>
2024-09-07 21:23:56 +0530

    Added ToDo with toggle button

--- react/todo-app/src/App.tsx
+++ react/todo-app/src/App.tsx
@@ -1,14 +1,14 @@
-import React, { useState } from 'react'
-import './App.css'
+import React, { useState } from 'react';
+import './App.css';
 
-interface todoAppItem {
-  title: string | undefined,
-  description: string | undefined,
-  completed: boolean | undefined | string
+interface TodoAppItem {
+  title: string;
+  description: string;
+  completed: boolean;
 }
 
 const App: React.FC = (): JSX.Element => {
-  const [todos, setTodos] = useState<Array<todoAppItem | null>>([{
+  const [todos, setTodos] = useState<Array<TodoAppItem>>([{
     title: "Go to Gym",
     description: "5-7 Gym",
     completed: false
@@ -16,31 +16,46 @@ const App: React.FC = (): JSX.Element => {
     title: "Hello World",
     description: "Learn ASP / Spring / JS-TS",
     completed: true
-  }])
+  }]);
+
+  const toggleTodoStatus = (index: number): void => {
+    const updatedTodos = [...todos];
+    updatedTodos[index].completed = !updatedTodos[index].completed;
+    setTodos(updatedTodos);
+  };
 
   return (
     <div>
       {todos.map((todo, index) => (
         <TodoItem
           key={index}
-          title={todo?.title}
-          description={todo?.description}
-          completed={JSON.stringify(todo?.completed)}
+          title={todo.title}
+          description={todo.description}
+          completed={todo.completed}
+          toggleCompleted={() => toggleTodoStatus(index)}
         />
       ))}
     </div>
-  )
-
+  );
 }
 
-const TodoItem: React.FC<todoAppItem> = (props: todoAppItem):JSX.Element => {
+interface TodoItemProps {
+  title: string;
+  description: string;
+  completed: boolean;
+  toggleCompleted: () => void;
+}
+
+const TodoItem: React.FC<TodoItemProps> = ({ title, description, completed, toggleCompleted }): JSX.Element => {
   return (
     <div>
-      <h1>{props.title}</h1>
-      <p>{props.description}</p>
-      <p>{props.completed}</p>
+      <h1>{title}</h1>
+      <p>{description}</p>
+      <button onClick={toggleCompleted}>
+        {completed ? "Completed" : "Mark as complete"}
+      </button>
     </div>
-  )
-}
+  );
+};
 
-export default App
+export default App;
