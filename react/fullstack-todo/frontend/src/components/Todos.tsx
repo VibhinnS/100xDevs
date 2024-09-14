5b514473a8e072a359cf185e0822f9ad851827d1
VibhinnS <vibhinnsinghal25@gmail.com>
2024-09-14 13:11:08 +0530

    Added TodoItem component, also added complete and mark complete toggle

--- react/fullstack-todo/frontend/src/components/Todos.tsx
+++ react/fullstack-todo/frontend/src/components/Todos.tsx
@@ -1,16 +1,11 @@
-import React from 'react'
 import { ITodoItem, ITodoProps } from '../interface'
+import TodoItem from './TodoItem';
 
 const Todos: React.FC<ITodoProps> = ({ todos }): JSX.Element => {
   return (
     <div>
       {todos.map((todo: ITodoItem, index: number) => (
-        <div key={index}>
-          <h1>{todo.title}</h1>
-          <h2>{todo.description}</h2>
-          <h2>{JSON.stringify(todo.targetDate)}</h2>
-          <button>{todo.completed ? "Completed" : "Mark as Completed"}</button>
-        </div>
+        <TodoItem key={index} title={todo.title} description={todo.description} targetDate={todo.targetDate} completed={todo.completed}/>
       ))}
     </div>
   );
